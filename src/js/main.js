5ae5ec6b9f9875459f2d8fcf080bfaaa72258b12
Yugsolanki <yugsolanki15@gmail.com>
2022-08-26 21:56:45 +0530

    changed landing page without gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import { gsap } from "gsap";
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 //cursor
 
@@ -10,10 +13,60 @@ document.body.addEventListener("mousemove", event => {
         x: mouseX,
         y: mouseY
     })
-
-    gsap.to(".shape", {
-        x: mouseX,
-        y: mouseY,
-        stagger: -0.1
-    });
 });
+
+
+let logo = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.landing-page',
+      start: "top top",
+      //pin: true,
+      endTrigger: '#pixel-logo',
+      end: "top 50%",
+      //markers: true,
+      scrub: 1
+    }
+  });
+  
+  let buildings = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.landing-page',
+      start: "top top",
+      pin: true,
+      end: "bottom top",
+      //markers: true,
+      scrub: 1
+    }
+  });
+  
+  /* let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.content',
+      start: "top 75%",
+      end: "top 10%",
+      toggleActions: "restart reverse restart reverse",
+    }
+  });
+   */
+  
+  buildings.to('#buildings', {scale: 1.1, y: -20});
+  logo.to('#pixel-logo', {y: -450, scale: 2});
+  
+  gsap.from('.content h1', {
+    x: -400,
+    scrollTrigger: {
+      trigger: '.content',
+      start: "top 75%",
+      end: "top 10%",
+      toggleActions: "restart reverse restart reverse",
+    }
+  });
+  gsap.from('.content p', {
+    x: -1000,
+    scrollTrigger: {
+      trigger: '.content',
+      start: "top 75%",
+      end: "top 10%",
+      toggleActions: "restart reverse restart reverse",
+    }
+  });
